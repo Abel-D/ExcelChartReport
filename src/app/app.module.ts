1c41633fac895513ebff236cd1215c6f1874c99d
Abel D <70660536+Abel-D@users.noreply.github.com>
2022-05-22 15:04:21 +0300

    emp,proj components and chart service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ChartViewComponent } from './chart-view/chart-view.component';
+import { EmployeesSummaryComponent } from './employees-summary/employees-summary.component';
+import { ProjectSummaryComponent } from './project-summary/project-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartViewComponent
+    ChartViewComponent,
+    EmployeesSummaryComponent,
+    ProjectSummaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employees-summary/employees-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-summary',
+  templateUrl: './employees-summary.component.html',
+  styleUrls: ['./employees-summary.component.css']
+})
+export class EmployeesSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
