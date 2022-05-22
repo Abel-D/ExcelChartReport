d651e7250f9825bbb4d32476080ad8428adcfc13
Abel D <70660536+Abel-D@users.noreply.github.com>
2022-05-22 14:09:26 +0300

    antd added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChartViewComponent } from './chart-view/chart-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartViewComponent
   ],
   imports: [
     BrowserModule
