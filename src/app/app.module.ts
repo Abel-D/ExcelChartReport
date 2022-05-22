217a612dc4b5ac9216fd275f911c7f43dc5bd18e
Abel D <70660536+Abel-D@users.noreply.github.com>
2022-05-22 16:10:30 +0300

    added grid view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { ChartViewComponent } from './chart-view/chart-view.component';
+import { EmployeesSummaryComponent } from './employees-summary/employees-summary.component';
+import { ProjectSummaryComponent } from './project-summary/project-summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartViewComponent
+    ChartViewComponent,
+    EmployeesSummaryComponent,
+    ProjectSummaryComponent
   ],
   imports: [
     BrowserModule,
